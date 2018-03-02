6a0941fc1c0bf8b418fef5ea96ae00801c933130
[kcheng] <[771588005@qq.com]>
2018-03-02 09:26:16 +0800

    权限控制修改以及告警模块更改

--- monitor/src/main/java/com/zhongda/monitor/business/controller/AlarmController.java
+++ monitor/src/main/java/com/zhongda/monitor/business/controller/AlarmController.java
@@ -18,58 +18,61 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.WebUtils;
 
 import com.zhongda.monitor.account.model.User;
 import com.zhongda.monitor.account.security.RoleSign;
+import com.zhongda.monitor.account.utils.TokenUtils;
 import com.zhongda.monitor.business.model.Alarm;
 import com.zhongda.monitor.business.service.AlarmService;
 import com.zhongda.monitor.core.model.Result;
-
+import com.zhongda.monitor.core.utils.HeaderUtils;
 
 @RestController
 @RequestMapping("/alarm")
-@Api(tags = {"告警信息操作接口"})
+@Api(tags = { "告警信息操作接口" })
 public class AlarmController {
-	
-	@Resource 
+
+	@Resource
 	AlarmService alarmService;
-	
+
 	@GetMapping("/queryAlarm")
 	@ApiOperation(value = "分页按条件查询当前用户下的告警信息", httpMethod = "GET", response = Result.class, notes = "分页查询当前用户下的告警信息")
-	public	Result <Map <String, Object>> selectAlarmByUserId (Alarm alarm,
-		HttpServletRequest request){
+	public Result<Map<String, Object>> selectAlarmByUserId(Alarm alarm,
+			HttpServletRequest request) {
+		Subject subject = SecurityUtils.getSubject();
+		if (!subject.hasRole(RoleSign.ADMIN)
+				&& !subject.hasRole(RoleSign.SUPER_ADMIN)) {
+		// 非管理员用户，只可查看自己的告警信息，查询条件增加userId
+		String token = HeaderUtils.getTokenFromRequest(request);
+		User user = TokenUtils.getUserFromeToken(token);
+		alarm.setUserId(user.getUserId());
+		}
+		Map<String, Object> map = alarmService.selectPageAlarmByQuery(alarm);
+		Result<Map<String, Object>> result = new Result<Map<String, Object>>();
+		return result.success("查询成功", map);
+	}
+
+	@PutMapping("/{alarmId}")
+	@ApiOperation(value = "修改告警信息状态", httpMethod = "PUT", response = Result.class, notes = "修改告警信息状态")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "alarmId", value = "告警id", required = true, dataType = "int", paramType = "path") })
+	public Result<Alarm> selectAlarmByUserId(@PathVariable Integer alarmId) {
+		System.out.println("alarmId:" + alarmId);
+		return alarmService.updateAlarmStatusByAlarmId(alarmId);
+	}
+
+	@DeleteMapping("/deleteAlarm")
+	@ApiOperation(value = "删除告警信息   暂时没用上", httpMethod = "DELETE", response = Result.class, notes = "删除告警信息  暂时没用上")
+	public Result<String> deleteAlarm(Alarm alarm, HttpServletRequest request) {// 根据条件删除告警信息
 		Subject subject = SecurityUtils.getSubject();
 		if (!subject.hasRole(RoleSign.ADMIN)
 				&& !subject.hasRole(RoleSign.SUPER_ADMIN)) {
 			// 非管理员用户，只可查看自己的告警信息，查询条件增加userId
-			User user = (User) WebUtils.getSessionAttribute(request, "userInfo");
+			String token = HeaderUtils.getTokenFromRequest(request);
+			User user = TokenUtils.getUserFromeToken(token);
 			alarm.setUserId(user.getUserId());
 		}
-		Map <String, Object> map = alarmService.selectPageAlarmByQuery(alarm);
-		Result <Map <String, Object>> result = new Result <Map <String, Object>>();
-		return result.success("查询成功", map);
-	} 
-	
-	@PutMapping("/{alarmId}")
-	@ApiOperation(value = "修改告警信息状态", httpMethod = "PUT", response = Result.class, notes = "修改告警信息状态")
-	@ApiImplicitParams({ @ApiImplicitParam(name = "alarmId", value = "告警id", required = true, dataType = "int", paramType = "path") })	
-	public Result<Alarm> selectAlarmByUserId (@PathVariable Integer alarmId){
-		System.out.println("alarmId:"+alarmId);
-		return alarmService.updateAlarmStatusByAlarmId(alarmId);
-	}
-	
-	@DeleteMapping("/deleteAlarm")
-	@ApiOperation(value = "删除告警信息   暂时没用上", httpMethod = "DELETE", response = Result.class, notes = "删除告警信息  暂时没用上")
-	public Result <String> deleteAlarm(Alarm alarm ,HttpServletRequest request ){//根据条件删除告警信息
-		Subject subject = SecurityUtils.getSubject();
-		if (!subject.hasRole(RoleSign.ADMIN)
-				&& !subject.hasRole(RoleSign.SUPER_ADMIN)) {
-			// 非管理员用户，只可删除自己的告警信息，查询条件增加userId
-			User user = (User) WebUtils.getSessionAttribute(request, "userInfo");
-			alarm.setUserId(user.getUserId());
-		}
+		
 		return alarmService.deleteAlarm(alarm);
 	}
-	
+
 }
