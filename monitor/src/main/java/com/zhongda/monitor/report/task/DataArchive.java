e5776cc16c12b594a4c98b5c7f9688864a6ef234
huchao <1465703041@qq.com>
2018-04-18 14:39:00 +0800

    报告代码更新

--- monitor/src/main/java/com/zhongda/monitor/report/task/DataArchive.java
+++ monitor/src/main/java/com/zhongda/monitor/report/task/DataArchive.java
@@ -1,6 +1,8 @@
 package com.zhongda.monitor.report.task;
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,8 +17,10 @@ import com.zhongda.monitor.report.service.MigrationDataService;
 @Component
 public class DataArchive {
 	
+	private static final Logger logger = LoggerFactory.getLogger(DataArchive.class);
+	
 	@Autowired
-	  MigrationDataService migrationDataService;
+	 MigrationDataService migrationDataService;
 	  
 	
 	/**
@@ -27,22 +31,41 @@ public class DataArchive {
 	 */
 	@Scheduled(cron="0 0 00 * * ?")
 	public void archive() throws Exception {	//"2018-04-10 00:00:00", "2018-04-10 00:10:00"		
+		
 		DateTime start = DateTime.now();
+		
 		String beginTime = start.toString("YYYY-MM-dd HH:mm:ss");
-		String endTime = start.plusMinutes(10).toString("YYYY-MM-dd HH:mm:ss");		
+		
+		logger.info("现在是:"+beginTime+",准备数据归档");
+		
+		String endTime = start.plusMinutes(10).toString("YYYY-MM-dd HH:mm:ss");	
+		
 		migrationDataService.insertData(beginTime,endTime);
 	}	
+	
 	@Scheduled( cron="0 0 08 * * ?")
 	public void archive1() throws Exception {	
+		
 		DateTime start = DateTime.now();
+		
 		String beginTime = start.toString("YYYY-MM-dd HH:mm:ss");
+		
+		logger.info("现在是:"+beginTime+",准备数据归档");
+		
 		String endTime = start.plusMinutes(10).toString("YYYY-MM-dd HH:mm:ss");
+		
 		migrationDataService.insertData(beginTime,endTime);
 	}	
+	
 	@Scheduled( cron="0 0 16 * * ?")
 	public void archive2() throws Exception {
+		
 		DateTime start = DateTime.now();
+		
 		String beginTime = start.toString("YYYY-MM-dd HH:mm:ss");
+		
+		logger.info("现在是:"+beginTime+",准备数据归档");
+		
 		String endTime = start.plusMinutes(10).toString("YYYY-MM-dd HH:mm:ss");
 		
 		migrationDataService.insertData(beginTime,endTime);
