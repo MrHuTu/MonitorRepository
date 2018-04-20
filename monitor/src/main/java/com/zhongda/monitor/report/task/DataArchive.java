d89901ae624ffaa1768b4c06c20f3282111f88e5
huchao <1465703041@qq.com>
2018-04-20 11:14:47 +0800

    fix

--- monitor/src/main/java/com/zhongda/monitor/report/task/DataArchive.java
+++ monitor/src/main/java/com/zhongda/monitor/report/task/DataArchive.java
@@ -2,19 +2,12 @@ package com.zhongda.monitor.report.task;
 
 import java.util.Map;
 
-import org.joda.time.DateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 import com.zhongda.monitor.report.service.MigrationDataService;
 import com.zhongda.monitor.report.utils.GitYmlParaUtils;
-import com.zhongda.monitor.report.utils.SpringContextUtil;
 
 /**
  * 
